b9fcd3c55c24bdae2a0e49bb1d4c014dbb9bc152
Vinicius Guimarães <vinicius.dalmeidaguimaraes@gmail.com>
2017-06-25 17:52:29 -0300

    Exibe o número de colisões das duas tabelas/Hashsize precisa ser alterado para um número maior

--- NumeroColisoes.py
+++ NumeroColisoes.py
@@ -1,11 +1,11 @@
 import struct
 import hashlib
 
-hashSize = 15000001
+hashSize = 1000003
 fileName = "data/BolsaFamiliaJan.dat"
-cepFormat = "2s30s14s50s6s2s"
+fileFormat = "2s30s14s50s6s2s"
 keyColumnIndex = 2
-recordSize = struct.calcsize(cepFormat)
+recordSize = struct.calcsize(fileFormat)
 counts = [0] * hashSize
 
 def h2(key):
@@ -25,7 +25,49 @@ while True:
     line = f.read(recordSize)
     if line == "": # EOF
         break
-    record = struct.unpack(cepFormat, line)
+    record = struct.unpack(fileFormat, line)
+    p = h(record[keyColumnIndex])
+    counts[p] += 1
+    if counts[p] > 1:
+        colisoes += 1
+    if counts[p] > tamanhoMaiorLista:
+        tamanhoMaiorLista = counts[p]
+    recordCount += 1
+f.close()
+
+print "Numero Colisoes:", colisoes
+print "Tamanho Maior Lista:", tamanhoMaiorLista
+
+countOfCounts = [0] * (tamanhoMaiorLista+1)
+
+for i in counts:
+    countOfCounts[i] += 1
+
+print countOfCounts
+
+c = 0
+media = 0
+for j in countOfCounts:
+    probabilidade = c*(float(j)/recordCount)
+    print "Lista de tamanho", c, "probabilidade", probabilidade
+    media += c*probabilidade
+    c += 1
+
+print "Media acesso", media
+
+print ""
+
+fileName = "data/BolsaFamiliaFev.dat"
+
+colisoes = 0
+tamanhoMaiorLista = 0
+recordCount = 0
+f = open(fileName,"rb")
+while True:
+    line = f.read(recordSize)
+    if line == "": # EOF
+        break
+    record = struct.unpack(fileFormat, line)
     p = h(record[keyColumnIndex])
     counts[p] += 1
     if counts[p] > 1:
