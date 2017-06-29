4aa82557608f3a226accef3fcb731d240e1d274a
Vinicius Guimarães <vinicius.dalmeidaguimaraes@gmail.com>
2017-06-29 19:39:15 -0300

    Arquivo do bolsa familia de janeiro substituído por uma versão reduzida

--- forcaBruta.py
+++ forcaBruta.py
@@ -1,41 +1,51 @@
 #! /usr/bin/python
 
+"""Compara uma versão reduzida da tabela de janeiro com a tabela de fevereiro
+usando o método de força bruta"""
+
 import struct
 from time import time
 
 ti = time()
 estrutura = '2s30s14s50s6s2s'
 s = struct.Struct(estrutura)
-f1 = open('BolsaFamiliaJan.dat', 'rb')
-f2 = open('BolsaFamiliaFev.dat', 'rb')
+f1 = open('data/BolsaFamiliaJanReduzido.dat', 'rb')
+f2 = open('data/BolsaFamiliaFev.dat', 'rb')
 linha1 = f1.read(s.size)
 existem = 0
 naoExistem = 0
+totalAcessos = 0
+i = 0
 
 #Roda ate o fim do arquivo de janeiro
 while linha1 != b'':
+	i += 1
+
+	if i%1000 == 0:
+		print(str(i//1000) + "%")
 	dados1 = s.unpack(linha1)
+	f2.seek(0)
 	linha2 = f2.read(s.size)
 	
 	#Roda ate o fim do arquivo de fevereiro
 	while linha2 != b'':
+		totalAcessos += 1
 		dados2 = s.unpack(linha2)
 		
-		#Adiciona mais um no contador, caso o NIS seja igual, volta para o comeco do arquivo e para o loop
+		#Adiciona mais um no contador, caso o NIS seja igual, para o loop
 		if dados1[2] == dados2[2]:
 			existem += 1
-			f2.seek(0)
 			break
-		#Le a proxima linha, caso o NIS nao for igual, e caso seja o final do arquivo, adiciona mais um ao contador
+		#Le a proxima linha, caso o NIS nao seja igual, e caso seja o final do arquivo, adiciona mais um ao contador
 		else:
 			linha2 = f2.read(s.size)
 			if linha2 == b'':
 				naoExistem += 1
-				f2.seek(0)
 				break
 
 	linha1 = f1.read(s.size)
 
-print("Quantidade de pessoas que existem nas duas tabelas: " + str(existem))
-print("Quantidade de pessoas que existem na tabela de janeiro mas nao na de fevereiro: " + str(naoExistem))
+print("Quantidade de registros que existem nas duas tabelas: " + str(existem))
+print("Quantidade de registros que so existem na tabela de janeiro: " + str(naoExistem))
+print("Media de acessos: " + str(totalAcessos / 100000))
 print(time()-ti)
